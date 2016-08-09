7169fa6250b892a9c3ee2d31cd4d98b43874c139
wierje <wier.je@gmail.com>
2016-08-09 14:46:13 -0500

    javascript working

--- quiz.js
+++ quiz.js
@@ -8,36 +8,51 @@ var e = document.getElementById('button').addEventListener('click', checkInput)
 var f = document.getElementById('char').addEventListener('keyup', checkKeyPress)
 var g = document.getElementById('howTall').addEventListener('keyup', checkKeyPress)
 
+
 function checkKeyPress(event) {
-  if (event.keyCode == "13"){
-    checkInput()
-  }
+    if (event.keyCode == "13") {
+        checkInput()
+    }
 }
 
 function checkInput() {
-  var tree = {};
+    var tree = {};
 
     var check = parseInt(document.getElementById('howTall').value)
-    if (check === '') {
-        alert('Try again.')
+    if (isNaN(check)) {
+        alert('Try again. Use a number in the first box.')
     } else if (check > 20) {
         alert('A number between 1 and 20, please')
     } else if (check < 1) {
         alert('A number between 1 and 20, please')
     } else {
-        console.log(check)
         tree.height = check;
+        console.log(tree.height)
     }
 
 
     var checker = document.getElementById('char').value
     if (checker.length < 1) {
-        alert('Try again.')
+        alert('Try again. Fill in the character to use.')
     } else if (checker.length > 1) {
         alert('Just one key entry. Any key')
     } else {
-        console.log(checker)
         tree.character = checker;
+        console.log(tree.character)
     }
-        console.log(tree)
+    buildTree(tree)
+}
+
+function buildTree(tree) {
+    var treeHeight = tree.height
+    // console.log(treeHeight)
+    var treeCharacter = tree.character
+    // console.log(treeCharacter)
+    var counter = 1
+for (var i = 0; i < treeHeight; i++) {
+
+    console.log(' '.repeat(treeHeight-i) + treeCharacter.repeat(counter))
+        counter +=2
+}
+
 }
